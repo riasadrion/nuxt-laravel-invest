1fce765597702a6c67ea0a78f5584e14f81a1c3a
Riasad Rion <riasadrion@gmail.com>
2022-09-01 02:06:22 +0600

    axios

---
+++ frontend/layouts/auth.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container d-flex justify-content-center align-items-center login">
+        <div class="col-md-4">
+            <Nuxt />
+        </div>
+    </div>
+</template>
+
+<style>
+    .login {
+        height: 100vh;
+    }
+</style>
---
+++ frontend/pages/company/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="card">
+        <div class="card-header bg-primary text-white">Login</div>
+        <div class="card-body">
+          <div class="form-group">
+            <label for="">Username</label>
+            <input type="text" v-model="formData.username" class="form-control" />
+          </div>
+          <div class="form-group">
+            <label for="">Password</label>
+            <input type="password" v-model="formData.password" class="form-control" />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary btn-sm" @click="login">Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'auth',
+  data() {
+    return {
+      formData: {
+        username: 'admin',
+        password: 'admin123'
+      }
+    }
+  },
+  methods: {
+    login() {
+      alert('here')
+      this.$axios.post('login', this.formData)
+        .then(res => {
+          // localStorage.setItem('user', res.data.user);
+          // localStorage.setItem('accessToken', res.data.token);
+          // this.$router.push('/dashboard');
+        });
+    }
+  }
+}
+</script>
